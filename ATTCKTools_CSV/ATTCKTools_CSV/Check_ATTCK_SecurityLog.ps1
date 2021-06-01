1d8e3f58d585de2786252197a0494b26270c8eae
huymath2 <huymath@gmail.com>
2021-06-01 12:06:01 +0700

    Other ATTCK module updated

--- ATTCKTools_CSV/ATTCKTools_CSV/Check_ATTCK_SecurityLog.ps1
+++ ATTCKTools_CSV/ATTCKTools_CSV/Check_ATTCK_SecurityLog.ps1
@@ -4,16 +4,28 @@
 #--------------------------Result Array----------------------#
 
 #---Defense Evasion---#
-$T1070_ClearWindowsEventLogs = @()
+$EventID_1102 = @()
 $T1562_DisableorModifySystemFirewall = @()
-$T1562_DisableWindowsEventLogging = @()
-
+$EventID_1100 = @()
+$EventID_4768 = @()
+$EventID_4769 = @()
+$EventID_4928 = @()
+$EventID_4929 = @()
+$EventID_4765 = @()
+$EventID_4766 = @()
 
 #---------------------#
 
+#--Credential Access--#
+$EventID_4625 = @()
+$EventID_4648 = @()
+$EventID_4771 = @()
+$EventID_4624 = @()
+$EventID_4634 = @()
+$EventID_4672 = @()
 
 
-
+#---------------------#
 
 #------------------------------------------------------------#
 
@@ -30,8 +42,9 @@ Function Get-ATTCKSecurityLog{
             $report.Id = $_.Id
             $report.CreationTime = Get-Date -Date $_.TimeCreated -Format "yyyy-MM-dd HH:mm:ss"
             $report.Message = $_.Message
-            $global:T1070_ClearWindowsEventLogs += $report
+            $global:EventID_1102 += $report
         }
+
         if($_.Id -eq "4950" -or $_.Id -eq "4946" -or $_.Id -eq "4947" -or $_.Id -eq "4948" -or $_.Id -eq "4954" -or $_.Id -eq "4956" -or $_.Id -eq "5025" -or $_.Id -eq "5034"){
             $report = "" | Select-Object Id, CreationTime, Message       
             $report.Id = $_.Id
@@ -39,12 +52,61 @@ Function Get-ATTCKSecurityLog{
             $report.Message = $_.Message
             $global:T1562_DisableorModifySystemFirewall += $report
         }
+
         if($_.Id -eq "1100"){
             $report = "" | Select-Object Id, CreationTime, Message       
             $report.Id = $_.Id
             $report.CreationTime = Get-Date -Date $_.TimeCreated -Format "yyyy-MM-dd HH:mm:ss"
             $report.Message = $_.Message
-            $global:T1562_DisableWindowsEventLogging += $report
+            $global:EventID_1100 += $report
+        }
+
+        if($_.Id -eq "4768"){
+            $report = "" | Select-Object Id, CreationTime, Message       
+            $report.Id = $_.Id
+            $report.CreationTime = Get-Date -Date $_.TimeCreated -Format "yyyy-MM-dd HH:mm:ss"
+            $report.Message = $_.Message
+            $global:EventID_4768 += $report
+        }
+
+        if($_.Id -eq "4769"){
+            $report = "" | Select-Object Id, CreationTime, Message       
+            $report.Id = $_.Id
+            $report.CreationTime = Get-Date -Date $_.TimeCreated -Format "yyyy-MM-dd HH:mm:ss"
+            $report.Message = $_.Message
+            $global:EventID_4769 += $report
+        }
+
+        if($_.Id -eq "4928"){
+            $report = "" | Select-Object Id, CreationTime, Message       
+            $report.Id = $_.Id
+            $report.CreationTime = Get-Date -Date $_.TimeCreated -Format "yyyy-MM-dd HH:mm:ss"
+            $report.Message = $_.Message
+            $global:EventID_4928 += $report
+        }
+
+        if($_.Id -eq "4929"){
+            $report = "" | Select-Object Id, CreationTime, Message       
+            $report.Id = $_.Id
+            $report.CreationTime = Get-Date -Date $_.TimeCreated -Format "yyyy-MM-dd HH:mm:ss"
+            $report.Message = $_.Message
+            $global:EventID_4929 += $report
+        }
+
+        if($_.Id -eq "4765"){
+            $report = "" | Select-Object Id, CreationTime, Message       
+            $report.Id = $_.Id
+            $report.CreationTime = Get-Date -Date $_.TimeCreated -Format "yyyy-MM-dd HH:mm:ss"
+            $report.Message = $_.Message
+            $global:EventID_4765 += $report
+        }
+
+        if($_.Id -eq "4766"){
+            $report = "" | Select-Object Id, CreationTime, Message       
+            $report.Id = $_.Id
+            $report.CreationTime = Get-Date -Date $_.TimeCreated -Format "yyyy-MM-dd HH:mm:ss"
+            $report.Message = $_.Message
+            $global:EventID_4766 += $report
         }
 
         #Credential Access
@@ -53,7 +115,47 @@ Function Get-ATTCKSecurityLog{
             $report.Id = $_.Id
             $report.CreationTime = Get-Date -Date $_.TimeCreated -Format "yyyy-MM-dd HH:mm:ss"
             $report.Message = $_.Message
-            $global:T1562_DisableWindowsEventLogging += $report
+            $global:EventID_4625 += $report
+        }
+
+        if($_.Id -eq "4648"){
+            $report = "" | Select-Object Id, CreationTime, Message       
+            $report.Id = $_.Id
+            $report.CreationTime = Get-Date -Date $_.TimeCreated -Format "yyyy-MM-dd HH:mm:ss"
+            $report.Message = $_.Message
+            $global:EventID_4648 += $report
+        }
+
+        if($_.Id -eq "4771"){
+            $report = "" | Select-Object Id, CreationTime, Message       
+            $report.Id = $_.Id
+            $report.CreationTime = Get-Date -Date $_.TimeCreated -Format "yyyy-MM-dd HH:mm:ss"
+            $report.Message = $_.Message
+            $global:EventID_4771 += $report
+        }
+
+        if($_.Id -eq "4624"){
+            $report = "" | Select-Object Id, CreationTime, Message       
+            $report.Id = $_.Id
+            $report.CreationTime = Get-Date -Date $_.TimeCreated -Format "yyyy-MM-dd HH:mm:ss"
+            $report.Message = $_.Message
+            $global:EventID_4624 += $report
+        }
+
+        if($_.Id -eq "4634"){
+            $report = "" | Select-Object Id, CreationTime, Message       
+            $report.Id = $_.Id
+            $report.CreationTime = Get-Date -Date $_.TimeCreated -Format "yyyy-MM-dd HH:mm:ss"
+            $report.Message = $_.Message
+            $global:EventID_4634 += $report
+        }
+
+        if($_.Id -eq "4672"){
+            $report = "" | Select-Object Id, CreationTime, Message       
+            $report.Id = $_.Id
+            $report.CreationTime = Get-Date -Date $_.TimeCreated -Format "yyyy-MM-dd HH:mm:ss"
+            $report.Message = $_.Message
+            $global:EventID_4672 += $report
         }
          
     }
@@ -66,17 +168,42 @@ Get-ATTCKSecurityLog "$sdir\Security_Log0.csv"
 
 
 
-#---------------Export CSV-----------------------#
-$T1070_ClearWindowsEventLogs | Export-Csv "$sdir\T1070_ClearWindowsEventLogs.csv"
+<#---------------Export CSV-----------------------#
+$EventID_1102 | Export-Csv "$sdir\EventID_1102.csv"
 $T1562_DisableorModifySystemFirewall | Export-Csv "$sdir\T1562_DisableorModifySystemFirewall.csv"
-$T1562_DisableWindowsEventLogging | Export-Csv "$sdir\T1562_DisableWindowsEventLogging.csv"
+$EventID_1100 | Export-Csv "$sdir\EventID_1100.csv"
+$EventID_4768 | Export-Csv "$sdir\EventID_4768.csv"
+$EventID_4769 | Export-Csv "$sdir\EventID_4769.csv"
+$EventID_4928 | Export-Csv "$sdir\EventID_4928.csv"
+$EventID_4929 | Export-Csv "$sdir\EventID_4929.csv"
+$EventID_4765 | Export-Csv "$sdir\EventID_4765.csv"
+$EventID_4766 | Export-Csv "$sdir\EventID_4766.csv"
 
-#------------------------------------------------#
+$EventID_4625 | Export-Csv "$sdir\EventID_4625.csv"
+$EventID_4648 | Export-Csv "$sdir\EventID_4648.csv"
+$EventID_4771 | Export-Csv "$sdir\EventID_4771.csv"
+$EventID_4624 | Export-Csv "$sdir\EventID_4624.csv"
+$EventID_4634 | Export-Csv "$sdir\EventID_4634.csv"
+$EventID_4672 | Export-Csv "$sdir\EventID_4672.csv"
+#------------------------------------------------#>
 
 
-<#---------------Write on Console-----------------#
-$T1070_ClearWindowsEventLogs | FT -Property @{e = '*'; width = 30} -Wrap
+#---------------Write on Console-----------------#
+$EventID_1102 | FT -Property @{e = '*'; width = 30} -Wrap
 $T1562_DisableorModifySystemFirewall | FT -Property @{e = '*'; width = 30} -Wrap
-$T1562_DisableWindowsEventLogging | FT -Property @{e = '*'; width = 30} -Wrap
+$EventID_1100 | FT -Property @{e = '*'; width = 30} -Wrap
+$EventID_4768 | FT -Property @{e = '*'; width = 30} -Wrap
+$EventID_4769 | FT -Property @{e = '*'; width = 30} -Wrap
+$EventID_4928 | FT -Property @{e = '*'; width = 30} -Wrap
+$EventID_4929 | FT -Property @{e = '*'; width = 30} -Wrap
+$EventID_4765 | FT -Property @{e = '*'; width = 30} -Wrap
+$EventID_4766 | FT -Property @{e = '*'; width = 30} -Wrap
+
+$EventID_4625 | FT -Property @{e = '*'; width = 30} -Wrap
+$EventID_4648 | FT -Property @{e = '*'; width = 30} -Wrap
+$EventID_4771 | FT -Property @{e = '*'; width = 30} -Wrap
+$EventID_4624 | FT -Property @{e = '*'; width = 30} -Wrap
+$EventID_4634 | FT -Property @{e = '*'; width = 30} -Wrap
+$EventID_4672 | FT -Property @{e = '*'; width = 30} -Wrap
 
 #------------------------------------------------#>
---
+++ ATTCKTools_CSV/ATTCKTools_CSV/Review_ATTCK_Others.ps1
@@ -0,0 +1,43 @@
+﻿$ErrorActionPreference= 'silentlycontinue'
+
+function Pause ($Message = "Press any key to continue . . . ") {
+    if ((Test-Path variable:psISE) -and $psISE) {
+        $Shell = New-Object -ComObject "WScript.Shell"
+        $Button = $Shell.Popup("Click OK to continue.", 0, "Script Paused", 0)
+    }
+    else {     
+        Write-Host -NoNewline $Message
+        [void][System.Console]::ReadKey($true)
+        Write-Host
+    }
+}
+
+
+Function CheckExist{
+    while (1){
+        if(!(Test-Path -Path "$sdir\T1197_BITSJob.csv" -PathType Leaf)){
+            continue
+        }
+        $Readable = $false
+        try {
+            [System.IO.File]::OpenRead("$sdir\T1197_BITSJob.csv").Close()
+            $Readable = $true
+        }
+        catch {
+            $Readable = $false        
+        }
+        if($Readable){
+            break
+        }
+        
+    }
+
+}
+
+$sdir = $args[0]
+
+#Function follow PS logs
+Function Review_T1070_ClearCommandHistory{
+    
+    
+}
