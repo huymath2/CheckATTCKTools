5b662d9cb4ea6f2bb5c2fbb946c035f301c18d92
huymath2 <huymath@gmail.com>
2021-06-02 18:14:39 +0700

    02/06/2021

--- ATTCKTools_CSV/ATTCKTools_CSV/Check_ATTCK_DefenseEvasion.ps1
+++ ATTCKTools_CSV/ATTCKTools_CSV/Check_ATTCK_DefenseEvasion.ps1
@@ -4,11 +4,11 @@
 Function Get-WindowDefendLog{
     Get-WinEvent -LogName "Microsoft-Windows-Windows Defender/Operational" | Select-Object Id, TimeCreated, Message | ForEach-Object{
         if($_.Id -eq "5001" -or $_.Id -eq "5010" -or $_.Id -eq "5012"){
-            $report = "" | Select-Object Id, CreationTime, Event, Message       
+            $report = "" | Select-Object Id, CreationTime, Message       
             $report.Id = $_.Id
             $report.CreationTime = Get-Date -Date $_.TimeCreated -Format "yyyy-MM-dd HH:mm:ss"
             $report.Message = $_.Message
-			$report.Event = $_.Message.Split("`n")[0]
+			#$report.Event = $_.Message.Split("`n")[0]
             $report
         }
     }
@@ -39,7 +39,8 @@ $sdir = $args[0]
 Get-WindowDefendLog | Export-Csv "$sdir\T1562_DisableorModifyTools.csv"
 Get-HiddenFileAndDir | Export-Csv "$sdir\T1564_HiddenFilesandDirectories.csv"
 Get-RighttoLeftOverride | Export-Csv "$sdir\T1036_RighttoLeftOverride.csv"
-#call syscheck.bat -nobanner -tuv > "%sdir%\T1553_InstallRootCertificate.txt"
+#call syscheck.bat -nobanner -tuv > "%sdir%\T1553_InstallRootCertificate.txt" #>
 
+#Get-WindowDefendLog
 
 
--- ATTCKTools_CSV/ATTCKTools_CSV/Review_ATTCK_Others.ps1
+++ ATTCKTools_CSV/ATTCKTools_CSV/Review_ATTCK_Others.ps1
@@ -14,13 +14,19 @@ function Pause ($Message = "Press any key to continue . . . ") {
 
 
 Function CheckExist{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]$FilePath
+    )
+    
     while (1){
-        if(!(Test-Path -Path "$sdir\T1197_BITSJob.csv" -PathType Leaf)){
+        if(!(Test-Path -Path $FilePath -PathType Leaf)){
             continue
         }
         $Readable = $false
         try {
-            [System.IO.File]::OpenRead("$sdir\T1197_BITSJob.csv").Close()
+            [System.IO.File]::OpenRead($FilePath).Close()
             $Readable = $true
         }
         catch {
@@ -36,8 +42,155 @@ Function CheckExist{
 
 $sdir = $args[0]
 
-#Function follow PS logs
+
+#Functions follow PS logs
 Function Review_T1070_ClearCommandHistory{
-    
-    
+    CheckExist("$sdir\T1070_ClearCommandHistory.csv")
+    $report = Import-Csv -Path "$sdir\T1070_ClearCommandHistory.csv"
+    $report
 }
+
+Function Review_T1562_IndicatorBlocking{
+    CheckExist("$sdir\T1562_IndicatorBlocking.csv")
+    $report = Import-Csv -Path "$sdir\T1562_IndicatorBlocking.csv"
+    $report
+
+}
+
+Function Review_T1562_ImpairCommandHistoryLogging{
+    CheckExist("$sdir\T1562_ImpairCommandHistoryLogging.csv")
+    $report = Import-Csv -Path "$sdir\T1562_ImpairCommandHistoryLogging.csv"
+    $report
+
+}
+
+Function Review_T1564_HiddenWindow{
+    CheckExist("$sdir\T1564_HiddenWindow.csv")
+    $report = Import-Csv -Path "$sdir\T1564_HiddenWindow.csv"
+    $report
+
+}
+
+Function Review_TA0007_Discovery{
+    CheckExist("$sdir\TA0007_Discovery.csv")
+    $report = Import-Csv -Path "$sdir\TA0007_Discovery.csv"
+    $report
+
+}
+
+
+#Funtions not follow logs
+Function Review_T1562_DisableorModifyTools{
+    CheckExist("$sdir\T1562_DisableorModifyTools.csv")
+    $report = Import-Csv -Path "$sdir\T1562_DisableorModifyTools.csv"
+    $report
+
+}
+
+Function Review_T1564_HiddenFilesandDirectories{
+    CheckExist("$sdir\T1564_HiddenFilesandDirectories.csv")
+    $report = Import-Csv -Path "$sdir\T1564_HiddenFilesandDirectories.csv"
+    $report
+
+}
+
+Function Review_T1036_RighttoLeftOverride{
+    CheckExist("$sdir\T1036_RighttoLeftOverride.csv")
+    $report = Import-Csv -Path "$sdir\T1036_RighttoLeftOverride.csv"
+    $report
+
+}
+
+
+
+#Functions follow Security logs
+Function CheckSecurityEvent{
+    CheckExist("$sdir\EventID_1102.csv")
+    CheckExist("$sdir\T1562_DisableorModifySystemFirewall.csv")
+    CheckExist("$sdir\EventID_1100.csv")
+    CheckExist("$sdir\EventID_4768.csv")
+    CheckExist("$sdir\EventID_4769.csv")
+    CheckExist("$sdir\EventID_4928.csv")
+    CheckExist("$sdir\EventID_4929.csv")
+    CheckExist("$sdir\EventID_4765.csv")
+    CheckExist("$sdir\EventID_4766.csv")
+    CheckExist("$sdir\EventID_4625.csv")
+    CheckExist("$sdir\EventID_4648.csv")
+    CheckExist("$sdir\EventID_4771.csv")
+    CheckExist("$sdir\EventID_4624.csv")
+    CheckExist("$sdir\EventID_4634.csv")
+    CheckExist("$sdir\EventID_4672.csv")
+
+}
+
+Function Review_T1134_SID-HistoryInjection{
+    $report = Import-Csv -Path "$sdir\T1036_RighttoLeftOverride.csv"
+
+}
+
+
+
+
+#-----------------------PowerShell Log---------------------------------#
+#CreationTime, EventId, Description, ProviderName, HostName, HostApplication
+Write-Host "[+] Ra soat Clear Command History..."
+Review_T1070_ClearCommandHistory | Format-Table -Property @{e = "CreationTime"; width = 20}, @{e = "EventId"; width = 10}, @{e = "Description"; width = 30},  @{e = "ProviderName"; width = 20}, @{e = "HostName"; width = 10}, @{e = "HostName"; width = 20} -Wrap | Out-String -Width 2048 | more
+
+Pause
+
+Write-Host "[+] Ra soat Indicator Blocking..."
+Review_T1070_ClearCommandHistory | Format-Table -Property @{e = "CreationTime"; width = 20}, @{e = "EventId"; width = 10}, @{e = "Description"; width = 30},  @{e = "ProviderName"; width = 20}, @{e = "HostName"; width = 10}, @{e = "HostName"; width = 20} -Wrap | Out-String -Width 2048 | more
+
+Pause
+
+Write-Host "[+] Ra soat Impair Command History Logging..."
+Review_T1562_ImpairCommandHistoryLogging | Format-Table -Property @{e = "CreationTime"; width = 20}, @{e = "EventId"; width = 10}, @{e = "Description"; width = 30},  @{e = "ProviderName"; width = 20}, @{e = "HostName"; width = 10}, @{e = "HostName"; width = 20} -Wrap | Out-String -Width 2048 | more
+
+Pause
+
+Write-Host "[+] Ra soat Hidden Window..."
+Review_T1564_HiddenWindow | Format-Table -Property @{e = "CreationTime"; width = 20}, @{e = "EventId"; width = 10}, @{e = "Description"; width = 30},  @{e = "ProviderName"; width = 20}, @{e = "HostName"; width = 10}, @{e = "HostName"; width = 20} -Wrap | Out-String -Width 2048 | more
+
+Pause
+
+Write-Host "[+] Ra soat Discovery..."
+Review_TA0007_Discovery | Format-Table -Property @{e = "CreationTime"; width = 20}, @{e = "EventId"; width = 10}, @{e = "Description"; width = 30},  @{e = "ProviderName"; width = 20}, @{e = "HostName"; width = 10}, @{e = "HostName"; width = 20} -Wrap | Out-String -Width 2048 | more
+
+Pause
+
+#----------------------------------------------------------------------#
+
+#------------------------not in PowerShell Log-------------------------#
+Write-Host "[+] Ra soat Disable or Modify Tools..."
+Review_T1562_DisableorModifyTools | Format-Table -Wrap | Out-String -Width 2048 | more
+
+Pause
+
+Write-Host "[+] Ra soat Hidden Files and Directories..."
+Review_T1564_HiddenFilesandDirectories | Format-Table -Wrap | Out-String -Width 2048 | more
+
+Pause
+
+Write-Host "[+] Ra soat Right to Left Override..."
+Review_T1036_RighttoLeftOverride | Format-Table -Wrap | Out-String -Width 2048 | more
+
+
+#----------------------------------------------------------------------#
+
+
+#-------------------------Security Log---------------------------------#
+Write-Host "[+] Wait for all security logs..."
+CheckSecurityEvent
+
+Write-Host "[+] Ra soat Disable or Modify Tools..."
+Review_T1562_DisableorModifyTools | Format-Table -Wrap | Out-String -Width 2048 | more
+
+Pause
+
+
+
+
+
+
+
+#---------------------------------------------------------------------#
